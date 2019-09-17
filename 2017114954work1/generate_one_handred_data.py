d2893f6e72fd21c6658d1bda87ae77c75c95d439
2leimeng <ixlm37@163.com>
2019-09-17 18:51:07 +0800

    specification

--- 2017114954work1/generate_one_handred_data.py
+++ 2017114954work1/generate_one_handred_data.py
@@ -11,9 +11,9 @@ norange=np.array([np.random.normal(0,1,10),np.random.normal(1,1,10)])
 s=np.random.uniform(0,1,100)
 #step2-2,2-3
 for i in s:
-    subscript=int(10*i)-1
-    blue.append([np.random.normal(nblue[0][subscript],0.2),np.random.normal(nblue[1][subscript],0.2)])
-    orange.append([np.random.normal(norange[0][subscript],0.2),np.random.normal(norange[1][subscript],0.2)])
+    index=int(10*i)-1
+    blue.append([np.random.normal(nblue[0][index],0.2),np.random.normal(nblue[1][index],0.2)])
+    orange.append([np.random.normal(norange[0][index],0.2),np.random.normal(norange[1][index],0.2)])
 
 blue=np.array(blue)
 orange=np.array(orange)
