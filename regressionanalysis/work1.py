b22e8b39bb1eb7a2c651c9a8b872a9c52c2f4aea
2leimeng <ixlm37@163.com>
2019-09-17 12:29:00 +0800

    regression analysis

---
+++ regressionanalysis/work1.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([825, 215, 1070, 550, 480, 920, 1350, 325, 670, 1215])
+y = np.array([3.5, 1.0, 4.0, 2.0, 1.0, 3.0, 4.5, 1.5, 3.0, 5.0])
+# 1)
+plt.scatter(x,y)
+plt.savefig('scatter_figure.png')
+# 3)
+def least_square(x,y):
+    sumx2 = sum(x ** 2)
+    sumxy = sum(x * y)
+    meanx = sum(x) / x.shape[0]
+    meany = sum(y) / y.shape[0]
+    n = x.shape[0]
+
+    beta1 = (sumxy - n * meanx * meany) / (sumx2 - n * meanx ** 2)
+    beta0 = meany - beta1 * meanx
+
+    return beta0, beta1
+
+beta0, beta1 = least_square(x, y)
+
+X = np.arange(100, 1500, 100)
+Y = np.array([beta0 + beta1 * x for x in X])
+plt.plot(X,Y)
+plt.savefig("fit_figure.png")
+plt.close()
+# 4)
+sigma2 = []
+for valuey, valueY in zip(y,Y):
+    sigma2.append((valueY - valuey) ** 2)
+sumsigma2 = sum(sigma2)
+# 5)
+
+# 6)
+def SSR(Y,y):
+    meany = sum(y) / y.shape[0]
+    for i in Y:
+        yield (i - meany) ** 2
+
+def SST(y):
+    meany = sum(y) / y.shape[0]
+    for i in y:
+        yield (i - meany) ** 2
+
+r2 = sum(SSR(Y,y)) / sum(SST(y))
+# 7)
+St = sum(SST(y))
+Sr = beta1 ** 2 * sum(SST(x))
+Se = St - Sr
+F_ratio = Sr / Se
+print(F_ratio)
+# F0.975(1,8) < F(1,8)=9.049481336581056 < F0.99(1,8)
+# 8)
+
+# 9)
+
+# 10)
+epsilon = np.array([yi - Yi for yi, Yi in zip(y,Y)])
+plt.scatter(y,epsilon)
+plt.savefig('epsilon_figure.png')
+# 11)
+x0 = 1000
+predict_y = beta0 + beta1 * x0
+
+# 12)
+
+# 13)
